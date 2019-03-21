7489740a7fc2df1642bbd7b0047b367ccd39dfbb
Phillip Booker <phillippbooker@gmail.com>
2019-03-20 18:19:21 -0700

    Adds multiples of guessed letters to answer display.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,8 @@ var progress = ["_", "_", "_", "_"];
 
 document.addEventListener("keyup", function(e){
     // console.log(e);
+    var answerIndex;
+
     var pressed = e.key;
     console.log(pressed);
     
@@ -14,11 +16,19 @@ document.addEventListener("keyup", function(e){
     var letterIndex = answer.indexOf(pressed);
     console.log(letterIndex);
 
+    
     if(answer.indexOf(pressed) < 0){
         guesses++;
     } else{
         console.log("Yes!");
-        progress[letterIndex] = answer[letterIndex]
+        progress[letterIndex] = answer[letterIndex];
+
+        for (var i = 0; i < answer.length; i++) {
+            if(answer.charAt(i) == pressed){
+                progress[i] = pressed;
+            }
+            //alert(str.charAt(i));
+        }
     }
     document.getElementById("gameDisplay").innerHTML = progress.join(" ");
     console.log(guesses);
