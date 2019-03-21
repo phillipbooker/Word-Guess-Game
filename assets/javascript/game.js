ab049100b2d0d61b72c97e2225e24cd027330590
Phillip Booker <phillippbooker@gmail.com>
2019-03-20 19:56:10 -0700

    Adds game parameter panels.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,25 @@
-var answer = "test";
+var bank = ["hair", "goose", "berry", "apple", "pies"];
+//var answer = "test";
+var answer = bank[Math.floor(Math.random() * bank.length)];
 var guesses = 0;
 var attempts = [];
 var win = 0;
-var progress = ["_", "_", "_", "_"];
+// var progress = ["_", "_", "_", "_"];
+var progress = [];
 
+for (var i = 0; i < answer.length; i++){
+    progress.push("_");
+}
+
+
+
+/*
+//Create Game object
+//Set/create private variables
+//Add word bank
+
+Choose a new word once player wins
+*/
 
 
 document.addEventListener("keyup", function(e){
@@ -13,14 +29,13 @@ document.addEventListener("keyup", function(e){
     console.log(pressed);
     
     
-    
+    //Check to see if a letter was guessed
     var letterIndex = answer.indexOf(pressed);
     console.log(letterIndex);
 
     
     if((win == 0) && (answer.indexOf(pressed) < 0) && (attempts.indexOf(pressed) < 0)){
         attempts.push(pressed);
-        // console.log("Live: " + live);
         guesses++;
     } else{
         console.log("Yes!");
@@ -33,13 +48,14 @@ document.addEventListener("keyup", function(e){
         }
     }
     document.getElementById("gameDisplay").innerHTML = progress.join(" ");
+    document.getElementById("gameAttempts").innerHTML = attempts.join(", ");
+    document.getElementById("gameGuesses").innerHTML = "Guesses: " + guesses;
     console.log(attempts);
 
     if (progress.indexOf("_") < 0){
         win = 1;
         document.getElementById("gameWin").innerHTML = "You win!";
     };
-    //Check to see if "_" is still in progress
 
 });
 
